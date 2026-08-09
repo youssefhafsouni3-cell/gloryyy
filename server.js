41dbab82be50a2784b538ecd2fd6c554be708573
youssefhafsouni3-cell <youssefhafsouni3@gmail.com>
2026-08-09 20:07:23 +0200

    server.js

--- server.js
+++ server.js
@@ -135,6 +135,6 @@ app.post('/api/categories', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });

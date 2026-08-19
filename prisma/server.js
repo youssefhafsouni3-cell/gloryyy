6fa89b803c520ededc5953b613bd29e5d9d5e661
youssefhafsouni3-cell <youssefhafsouni3@gmail.com>
2026-08-19 16:11:39 +0200

    server.js

--- prisma/server.js
+++ prisma/server.js
@@ -19,14 +19,15 @@ const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
-  secure: false,
+  secure: false, 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
   tls: {
     rejectUnauthorized: false
-  }
+  },
+  connectionTimeout: 10000 // انتظار 10 ثوانٍ فقط للاتصال
 });
 
 // ==================== AUTHENTICATION ====================
